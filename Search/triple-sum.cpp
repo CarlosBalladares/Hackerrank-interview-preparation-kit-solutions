8fb8aad01555cb302f695562a475c0ebb1cf5b4d
Carlos Balladares <carlos@balladares.co>
2018-09-18 00:22:22 -0700

    Fixing I/O ugliness, this is why I call it Hackerrank junk see prev.

--- Search/triple-sum.cpp
+++ Search/triple-sum.cpp
@@ -1,4 +1,6 @@
 #include <bits/stdc++.h>
+#define FOR(x, n) for(int x = 0; x < n; x++)
+
 using namespace std;
 
 vector<string> split_string(string);
@@ -30,96 +32,39 @@ long triplets(vector<int> a, vector<int> b, vector<int> c) {
     return sol;
 }
 
-// ======== HACKERRANK's messy i/o =========
+// ======== I/O handling =========
 int main()
 {
-    ofstream fout(getenv("OUTPUT_PATH"));
-
-    string lenaLenbLenc_temp;
-    getline(cin, lenaLenbLenc_temp);
-
-    vector<string> lenaLenbLenc = split_string(lenaLenbLenc_temp);
-
-    int lena = stoi(lenaLenbLenc[0]);
-
-    int lenb = stoi(lenaLenbLenc[1]);
-
-    int lenc = stoi(lenaLenbLenc[2]);
-
-    string arra_temp_temp;
-    getline(cin, arra_temp_temp);
-
-    vector<string> arra_temp = split_string(arra_temp_temp);
-
-    vector<int> arra(lena);
-
-    for (int i = 0; i < lena; i++) {
-        int arra_item = stoi(arra_temp[i]);
-
-        arra[i] = arra_item;
-    }
-
-    string arrb_temp_temp;
-    getline(cin, arrb_temp_temp);
-
-    vector<string> arrb_temp = split_string(arrb_temp_temp);
-
-    vector<int> arrb(lenb);
-
-    for (int i = 0; i < lenb; i++) {
-        int arrb_item = stoi(arrb_temp[i]);
-
-        arrb[i] = arrb_item;
-    }
-
-    string arrc_temp_temp;
-    getline(cin, arrc_temp_temp);
-
-    vector<string> arrc_temp = split_string(arrc_temp_temp);
-
-    vector<int> arrc(lenc);
-
-    for (int i = 0; i < lenc; i++) {
-        int arrc_item = stoi(arrc_temp[i]);
-
-        arrc[i] = arrc_item;
-    }
-
-    long ans = triplets(arra, arrb, arrc);
-
-    fout << ans << "\n";
-
-    fout.close();
-
+    int len_a, len_b, len_c;
+    long temp;
+    
+    
+    cin >> len_a;
+    cin >> len_b;
+    cin >> len_c;
+    
+    vector<int> arr_a(len_a);
+    vector<int> arr_b(len_b);
+    vector<int> arr_c(len_c);
+    
+   
+    
+    FOR(i, len_a)
+        cin >> arr_a[i];
+    
+    FOR(i, len_b)
+        cin >> arr_b[i];
+    
+    FOR(i, len_c)
+        cin >> arr_c[i];
+    
+    
+    
+    long sol = triplets(arr_a, arr_b, arr_c);
+    
+    cout << sol << endl;
+    
+        
     return 0;
-}
-
-vector<string> split_string(string input_string) {
-    string::iterator new_end = unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
-        return x == y and x == ' ';
-    });
-
-    input_string.erase(new_end, input_string.end());
-
-    while (input_string[input_string.length() - 1] == ' ') {
-        input_string.pop_back();
-    }
-
-    vector<string> splits;
-    char delimiter = ' ';
-
-    size_t i = 0;
-    size_t pos = input_string.find(delimiter);
-
-    while (pos != string::npos) {
-        splits.push_back(input_string.substr(i, pos - i));
-
-        i = pos + 1;
-        pos = input_string.find(delimiter, i);
-    }
-
-    splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
-
-    return splits;
 }
 
