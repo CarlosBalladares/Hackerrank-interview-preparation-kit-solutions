cddbaac0b66b11ebf32e03f005610c94ab4a40d7
Carlos Balladares <carlos@balladares.co>
2018-09-19 21:20:46 -0700

    addint bound and fixing i/o

--- Search/maximum-subarray-sum.cpp
+++ Search/maximum-subarray-sum.cpp
@@ -12,6 +12,9 @@ using namespace std;
 vector<string> split_string(string);
 
 
+// Hackerrank maximum subarray sum Solution
+// Runs in O( n log(n-1) )
+// ======== SOLUTION =========
 long maximumSum(vector<long> a, long m) {
     
     int n = a.size();
@@ -35,72 +38,26 @@ long maximumSum(vector<long> a, long m) {
     return ( maximum );
 }
 
+// ======== I/O handling =========
 int main()
 {
-    ofstream fout(getenv("OUTPUT_PATH"));
+    int i, q;
+    long n, m;
 
-    int q;
     cin >> q;
-    cin.ignore(numeric_limits<streamsize>::max(), '\n');
-
-    for (int q_itr = 0; q_itr < q; q_itr++) {
-        string nm_temp;
-        getline(cin, nm_temp);
-
-        vector<string> nm = split_string(nm_temp);
-
-        int n = stoi(nm[0]);
-
-        long m = stol(nm[1]);
-
-        string a_temp_temp;
-        getline(cin, a_temp_temp);
-
-        vector<string> a_temp = split_string(a_temp_temp);
-
+    
+    while(q>0){
+        cin >> n;
+        cin >> m;
+        
         vector<long> a(n);
-
-        for (int i = 0; i < n; i++) {
-            long a_item = stol(a_temp[i]);
-
-            a[i] = a_item;
-        }
-
-        long result = maximumSum(a, m);
-
-        fout << result << "\n";
+        
+        for( i = 0; i< n; i++)
+            cin >> a[i];
+        
+        cout << maximumSum(a, m) <<endl;
+        
+        q--;
     }
-
-    fout.close();
-
-    return 0;
-}
-
-vector<string> split_string(string input_string) {
-    string::iterator new_end = unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
-        return x == y and x == ' ';
-    });
-
-    input_string.erase(new_end, input_string.end());
-
-    while (input_string[input_string.length() - 1] == ' ') {
-        input_string.pop_back();
-    }
-
-    vector<string> splits;
-    char delimiter = ' ';
-
-    size_t i = 0;
-    size_t pos = input_string.find(delimiter);
-
-    while (pos != string::npos) {
-        splits.push_back(input_string.substr(i, pos - i));
-
-        i = pos + 1;
-        pos = input_string.find(delimiter, i);
-    }
-
-    splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
-
-    return splits;
+    
 }
